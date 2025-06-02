e3e6817311b0ab9bc9aee28d1dc0c0a4bba8e5ce
Iurii Kravchenko <xjie6@ydb.tech>
2025-06-02 15:21:45 +0300

    fixup! fixup! fixup! fixup! fixup! cache size

--- ydb/core/tx/columnshard/data_accessor/node_actor.cpp
+++ ydb/core/tx/columnshard/data_accessor/node_actor.cpp
@@ -3,7 +3,7 @@
 namespace NKikimr::NOlap::NDataAccessorControl {
 
 NActors::IActor* TNodeActor::CreateActor() {
-    AFL_WARN(NKikimrServices::TX_COLUMNSHARD)("IURII", "CREATE");
+    // AFL_WARN(NKikimrServices::TX_COLUMNSHARD)("IURII", "CREATE");
     return new TNodeActor();
 }
 
